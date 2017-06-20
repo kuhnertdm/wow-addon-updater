d773e460a1429b008f79cacb8b5aee34233673a9
Sebastian Mischke <SebastianMischke@gmx.de>
2017-06-21 00:18:13 +0200

    Move config file reading to main

--- WoWAddonUpdater.py
+++ WoWAddonUpdater.py
@@ -6,6 +6,27 @@ from os.path import isfile
 def main():
     print('')
 
+    # Read config file
+
+    if not isfile('config.ini'):
+        print(
+            'Failed to read configuration file. Are you sure there is a file called "config.ini" with the "WowAddonUpdater.py" file?')
+        confirmExit()
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    try:
+        WOW_ADDON_LOCATION = config['WOW ADDON UPDATER']['WoW Addon Location']
+        ADDON_LIST_FILE = config['WOW ADDON UPDATER']['Addon List File']
+    except Exception:
+        print('Failed to parse configuration file. Are you sure it is formatted correctly?')
+        confirmExit()
+
+    if not isfile(ADDON_LIST_FILE):
+        print('Failed to read addon list file. Are you sure the file exists?')
+        confirmExit()
+
     return
 
 
@@ -17,28 +38,6 @@ if __name__ == "__main__":
 def confirmExit():
     input('\nPress the Enter key to exit')
     exit(0)
-
-
-# Read config file
-
-if not isfile('config.ini'):
-    print(
-        'Failed to read configuration file. Are you sure there is a file called "config.ini" with the "WowAddonUpdater.py" file?')
-    confirmExit()
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-try:
-    WOW_ADDON_LOCATION = config['WOW ADDON UPDATER']['WoW Addon Location']
-    ADDON_LIST_FILE = config['WOW ADDON UPDATER']['Addon List File']
-except Exception:
-    print('Failed to parse configuration file. Are you sure it is formatted correctly?')
-    confirmExit()
-
-if not isfile(ADDON_LIST_FILE):
-    print('Failed to read addon list file. Are you sure the file exists?')
-    confirmExit()
 
 
 def getAddon(ziploc):
