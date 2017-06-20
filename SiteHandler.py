ac836a9274b3d2dd083ef522862d13b52179827c
Sebastian Mischke <SebastianMischke@gmx.de>
2017-06-21 01:09:26 +0200

    Move curse fruntion to seperate file

---
+++ SiteHandler.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def curse(addonpage):
+    try:
+        page = requests.get(addonpage + '/download')
+        contentString = str(page.content)
+        indexOfZiploc = contentString.find('data-href') + 11  # Will be the index of the first char of the url
+        endQuote = contentString.find('"', indexOfZiploc)  # Will be the index of the ending quote after the url
+        return contentString[indexOfZiploc:endQuote]
+    except Exception:
+        print('Failed to find downloadable zip file for addon. Skipping...\n')
+        return ''
--- WoWAddonUpdater.py
+++ WoWAddonUpdater.py
@@ -56,22 +56,12 @@ class AddonUpdater:
             return
 
     def findZiploc(self, addonpage):
+        import SiteHandler
         if addonpage.startswith('https://mods.curse.com/addons/wow/'):
-            return self.__curse(addonpage)
+            return SiteHandler.curse(addonpage)
         else:
             print('Invalid addon page. Make sure you are using the Curse page for the addon.')
             confirmExit()
-
-    def __curse(self, addonpage):
-        try:
-            page = requests.get(addonpage + '/download')
-            contentString = str(page.content)
-            indexOfZiploc = contentString.find('data-href') + 11  # Will be the index of the first char of the url
-            endQuote = contentString.find('"', indexOfZiploc)  # Will be the index of the ending quote after the url
-            return contentString[indexOfZiploc:endQuote]
-        except Exception:
-            print('Failed to find downloadable zip file for addon. Skipping...\n')
-            return ''
 
 
 def main():
