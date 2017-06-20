6dd9f4fb62b101064e65f27ba6f7af490594b507
Derek Kuhnert <kuhnerdm@rose-hulman.edu>
2017-06-20 12:49:43 -0500

    Added some verbosity and extra error checking

--- WoWAddonUpdater.py
+++ WoWAddonUpdater.py
@@ -29,20 +29,34 @@ if(not isfile(ADDON_LIST_FILE)):
 	confirmExit()
 
 def getAddon(ziploc):
-	r = requests.get(ziploc, stream=True)
-	z = zipfile.ZipFile(BytesIO(r.content))
-	z.extractall(WOW_ADDON_LOCATION)
+	if(ziploc == ''):
+		return
+	try:
+		r = requests.get(ziploc, stream=True)
+		z = zipfile.ZipFile(BytesIO(r.content))
+		z.extractall(WOW_ADDON_LOCATION)
+	except Exception:
+		print('Failed to download or extract zip file for addon. Skipping...\n')
+		return
 
 def findZiploc(addonpage):
-	page = requests.get(addonpage + '/download')
-	contentString = str(page.content)
-	indexOfZiploc = contentString.find('data-href') + 11 # Will be the index of the first char of the url
-	endQuote = contentString.find('"', indexOfZiploc) # Will be the index of the ending quote after the url
-	return contentString[indexOfZiploc:endQuote]
+	if(not addonpage.startswith('https://mods.curse.com/addons/wow/')):
+		print('Invalid addon page. Make sure you are using the Curse page for the addon.')
+		confirmExit()
+	try:
+		page = requests.get(addonpage + '/download')
+		contentString = str(page.content)
+		indexOfZiploc = contentString.find('data-href') + 11 # Will be the index of the first char of the url
+		endQuote = contentString.find('"', indexOfZiploc) # Will be the index of the ending quote after the url
+		return contentString[indexOfZiploc:endQuote]
+	except Exception:
+		print('Failed to find downloadable zip file for addon. Skipping...\n')
+		return ''
 
 # Main process (yes I formatted the project badly)
 
 with open(ADDON_LIST_FILE, "r") as fin:
 	for line in fin:
+		print('Installing/updating addon: ' + line)
 		ziploc = findZiploc(line.rstrip('\n'))
 		getAddon(ziploc)
