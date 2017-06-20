89b7aa8fec6baf952cf8f7cb6006468fd1b81bc1
Sebastian Mischke <SebastianMischke@gmx.de>
2017-06-21 00:29:47 +0200

    Move main to AddonUpdater

--- WoWAddonUpdater.py
+++ WoWAddonUpdater.py
@@ -4,37 +4,6 @@ from os.path import isfile
 
 
 def main():
-    print('')
-
-    # Read config file
-
-    if not isfile('config.ini'):
-        print(
-            'Failed to read configuration file. Are you sure there is a file called "config.ini" with the "WowAddonUpdater.py" file?')
-        confirmExit()
-
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    try:
-        WOW_ADDON_LOCATION = config['WOW ADDON UPDATER']['WoW Addon Location']
-        ADDON_LIST_FILE = config['WOW ADDON UPDATER']['Addon List File']
-    except Exception:
-        print('Failed to parse configuration file. Are you sure it is formatted correctly?')
-        confirmExit()
-
-    if not isfile(ADDON_LIST_FILE):
-        print('Failed to read addon list file. Are you sure the file exists?')
-        confirmExit()
-
-    # Main process (yes I formatted the project badly)
-
-    with open(ADDON_LIST_FILE, "r") as fin:
-        for line in fin:
-            print('Installing/updating addon: ' + line)
-            ziploc = findZiploc(line.rstrip('\n'))
-            getAddon(ziploc, WOW_ADDON_LOCATION)
-
     return
 
 
@@ -53,7 +22,39 @@ class AddonUpdater:
         self.WOW_ADDON_LOCATION = ""
         return
 
-    def getAddon(ziploc, WOW_ADDON_LOCATION):
+    def main(self):
+        print('')
+
+        # Read config file
+
+        if not isfile('config.ini'):
+            print(
+                'Failed to read configuration file. Are you sure there is a file called "config.ini" with the "WowAddonUpdater.py" file?')
+            confirmExit()
+
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+
+        try:
+            WOW_ADDON_LOCATION = config['WOW ADDON UPDATER']['WoW Addon Location']
+            ADDON_LIST_FILE = config['WOW ADDON UPDATER']['Addon List File']
+        except Exception:
+            print('Failed to parse configuration file. Are you sure it is formatted correctly?')
+            confirmExit()
+
+        if not isfile(ADDON_LIST_FILE):
+            print('Failed to read addon list file. Are you sure the file exists?')
+            confirmExit()
+
+        # Main process (yes I formatted the project badly)
+
+        with open(ADDON_LIST_FILE, "r") as fin:
+            for line in fin:
+                print('Installing/updating addon: ' + line)
+                ziploc = findZiploc(line.rstrip('\n'))
+                getAddon(ziploc, WOW_ADDON_LOCATION)
+
+    def getAddon(self, ziploc, WOW_ADDON_LOCATION):
         if ziploc == '':
             return
         try:
@@ -64,7 +65,7 @@ class AddonUpdater:
             print('Failed to download or extract zip file for addon. Skipping...\n')
             return
 
-    def findZiploc(addonpage):
+    def findZiploc(self, addonpage):
         if not addonpage.startswith('https://mods.curse.com/addons/wow/'):
             print('Invalid addon page. Make sure you are using the Curse page for the addon.')
             confirmExit()
