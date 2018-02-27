3d955bca53b8aeaa5a287658c67856532c9ab1ca
Seamus Connor <seamushc@gmail.com>
2018-02-26 20:19:15 -0800

    Filter out empty lines in addon list file

--- WoWAddonUpdater.py
+++ WoWAddonUpdater.py
@@ -47,6 +47,8 @@ class AddonUpdater:
         with open(self.ADDON_LIST_FILE, "r") as fin:
             for line in fin:
                 line = line.rstrip('\n')
+                if not len(line):
+                    continue
                 currentVersion = SiteHandler.getCurrentVersion(line)
                 installedVersion = self.getInstalledVersion(line)
                 if not currentVersion == installedVersion:
